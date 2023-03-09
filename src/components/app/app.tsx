c2da2e02b2a5b4366f536100e74c7939ca8af7bf
Dmitry Khitrin <DmitryKhitrin@gmail.com>
2023-03-09 22:37:38 +0200

    Added base layout

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 
-import './App.css';
+import { ItemsList } from '../items-list';
+import { Map } from '../map/';
+
+import styles from './app.module.css';
 
 export const App = () => {
-
   return (
-    <div className="App">
-    </div>
+    <main className={styles.container}>
+      <div className={styles.sidebar}>
+        <ItemsList />
+      </div>
+
+      <section className={styles.content}>
+        <Map />
+      </section>
+    </main>
+
   );
 };
 
---
+++ src/components/items-list/items-list.tsx
@@ -0,0 +1,3 @@
+export const ItemsList = () => {
+  return null;
+};
