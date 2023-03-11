d8a0229267a2e97315b8e97fb2d06115686cda2a
Dmitry Khitrin <DmitryKhitrin@gmail.com>
2023-03-11 20:12:14 +0200

    Added map component

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,3 +1,19 @@
+import { MapContainer, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import styles from './map.module.css';
+
+
+
 export const Map = () => {
-  return null;
+  return (
+
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} className={styles.mapContainer}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+    </MapContainer>
+
+  );
 };
